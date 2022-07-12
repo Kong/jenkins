3ce1810edd2ae57c724551cb8dbe3818d118d6cf
Colin Hutchinson <chutchic@gmail.com>
2022-07-12 16:29:34 +0000

    chore(move): various changes as we move our jenkins

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-aws s3 sync s3://kong-jenkins-ssh /root/.ssh
+aws s3 sync s3://kong-aws-ecs-jenkins/.ssh /root/.ssh
 chmod 500 /root/.ssh/id_rsa
 git config --global user.name "Kong - Jenkins"
 git config --global user.email office@konghq.com
 ssh-keyscan -H github.com > /etc/ssh/ssh_known_hosts
-/sbin/tini -- /usr/local/bin/jenkins.sh
+/usr/bin/tini -- /usr/local/bin/jenkins.sh
