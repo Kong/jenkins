7931e1a1b6965701afc3caec3490e0ba4a0c1b99
Isa Farnik <isa@konghq.com>
2022-10-21 14:50:48 -0700

    fix(s3): rm SKIP_S3

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-if [ -n "${SKIP_S3:-}" ]; then
-  echo "skipping s3 sync"
-else
-  aws s3 sync s3://kong-aws-ecs-jenkins/.ssh /root/.ssh
-fi
+aws s3 sync s3://kong-aws-ecs-jenkins/.ssh /root/.ssh
 
 chmod 500 /root/.ssh/id_rsa
 git config --global user.name "Kong - Jenkins"
